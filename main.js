de8bcd0e5d005640bcf80622b0d95f2c4d3a8874
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-30 19:27:27 -0300

    Defined the rules for losing the game.

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 const grid = document.querySelector(".grid");
 const startBtn = document.querySelector("#start");
 const score = document.querySelector("#score");
+
+// Necessary variables
 const squares = [];
-const snake = [18, 17, 16];
+const snake = [2, 1, 0];
 let direction = 1;
+const timerId = setInterval(move, 500);
 
 // Event listeners
-startBtn.addEventListener("click", startGame);
+// startBtn.addEventListener("click", startGame);
+document.addEventListener("keydown", control);
 
 // Functions
 
@@ -24,11 +28,22 @@ snake.forEach((index) => squares[index].classList.add("snake"));
 
 // Make the snake move around the grid
 function move() {
+  if (
+    (snake[0] % 10 === 0 && direction === -1) ||
+    (snake[0] % 10 === 9 && direction === 1) ||
+    (snake[0] - 10 < 0 && direction === -10) ||
+    (snake[0] + 10 > 99 && direction === 10)
+  ) {
+    console.log("You hit a wall, and now lose!");
+    return clearInterval(timerId);
+  }
+
   const tail = snake.pop();
   squares[tail].classList.remove("snake");
 
   snake.unshift(snake[0] + direction);
   squares[snake[0]].classList.add("snake");
+  console.log(snake);
 }
 
 // Control snake movement
@@ -56,9 +71,7 @@ function control(event) {
 }
 
 //
-function startGame() {
-  const timerId = setInterval(move, 250);
-
-  document.addEventListener("keydown", control);
-  startBtn.removeEventListener("click", startGame);
-}
+// function startGame() {
+//   document.addEventListener("keydown", control);
+//   startBtn.removeEventListener("click", startGame);
+// }
